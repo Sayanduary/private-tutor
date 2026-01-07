74bb071972cd808c9ce8260cfcb99a738fdaa28b
Sayanduary <sayanduary@gmail.com>
2026-01-07 23:55:43 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import Hero from "../components/Hero";
+import About from "../components/About";
+import Subjects from "../components/Subjects";
+import Pricing from "../components/Pricing";
+import Contact from "../components/Contact";
 import PageWrapper from "../components/PageWrapper";
 
 export default function Home() {
   return (
     <PageWrapper>
       <Hero />
+      <About />
+      <Subjects />
+      <Pricing />
+      <Contact />
     </PageWrapper>
   );
 }
